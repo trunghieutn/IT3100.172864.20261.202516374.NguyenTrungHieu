4eb514896809520667622f64123a9734bfeb9a15
Hieu Nguyen <nth1132007@gmail.com>
2026-09-15 21:56:59 +0700

    Complete assignment 3.2.1

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+//Example 1: HelloWorld.java
+//Text-printing program
+public class HelloWorld{
+
+	public static void main(String args[]){
+		System.out.println("xin chao \n cac ban!");
+		System.out.println("Hello \t world!");
+
+	} // end of method main
+
+}
